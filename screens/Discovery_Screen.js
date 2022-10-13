68ca995fd77b142aea46c2f8163f32f0bf32235e
matt7696 <gengarodyssey@gmail.com>
2022-10-12 21:09:34 -0500

    uhhh

--- screens/Discovery_Screen.js
+++ screens/Discovery_Screen.js
@@ -3,12 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function DiscoveryScreen() {
     return ( 
       <View style={styles.container}>
-<<<<<<< HEAD:screens/Discovery_Screen.js
         <Text style={stylez.textColor}>Discovery Page</Text>
-=======
-        <Text>test screen guys!</Text>
-        <Text>this is a working app fr!</Text>
->>>>>>> 5b0da2ceb79393bc68b8c215392bbf2d3ea53d67:screens/test_screen.js
       </View>
     );
   }
