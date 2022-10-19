61aa85c35578140130d134e65f0da34756bbe226
matthew1 <gengarodyssey@gmail.com>
2022-10-18 20:17:22 -0500

    button stuff

--- screens/Discovery_Screen.js
+++ screens/Discovery_Screen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, Button } from 'react-native'
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native'
 
   //idk where to put these vars
   var songTitle = 'songTitleTemporary'
@@ -7,27 +7,29 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native'
 export default function DiscoveryScreen() {
     return ( 
       <View style={styles.container}>
+         
         <Text 
-            style={stylez.header}>
+            style={styles.header}>
             Discovery Page
         </Text>
         <Text 
-            style={stylez.songTitle}>
+            style={styles.songTitle}>
             {songTitle}
         </Text>
         <Text 
-            style={stylez.artist}>
+            style={styles.artist}>
             {artist}
         </Text>
         <Image 
-            style={stylez.image} 
+            style={styles.image} 
             source={require(albumCover)} 
         />
-        <Button 
-            style={stylez.homeButton}
-            title="Home"
-            color="#FFFFFF"
-        />
+        <TouchableOpacity 
+            style = {styles.homeButton}
+        >     
+        <Image source={require("../assets/icon.png")} style = {styles.homeButtonImage}/>
+        </TouchableOpacity>
+    
       </View>
 
     );
@@ -38,41 +40,43 @@ export default function DiscoveryScreen() {
     container: {
       flex: 1,
       backgroundColor: '#040f2f',
-      alignItems: 'center',
-      justifyContent: 'center',
+      flexDirection: 'column',
+      alignItems: ''
     },
-    })
-
-    const stylez = StyleSheet.create({
-        header: {
-            position: 'absolute',
-            left: 135,
-            top: 100,
-            fontSize: 17,
-            color: '#FFFFFF',
-        },
-        songTitle: {
-            position: 'absolute',
-            left: 49,
-            top: 486,
-            fontSize: 25,
-            color: '#FFFFFF',
-        },
-        artist: {
-            position: 'absolute',
-            left: 49,
-            top: 532,
-            fontSize: 15,
-            color: '#FFFFFF',
-        },
-        image: {
-            width: 300,
-            height: 300,
-            left: 0,
-            top: -100,
-        },
-        homeButton: {
-            top: 500,
-            left: 100,
-        }
+    header: {
+        left: 138,
+        top: 50,
+        fontSize: 17,
+        color: '#FFFFFF',
+    },
+    songTitle: {
+        position: 'absolute',
+        left: 49,
+        top: 486,
+        fontSize: 25,
+        color: '#FFFFFF',
+    },
+    artist: {
+        left: 49,
+        top: 532,
+        fontSize: 15,
+        color: '#FFFFFF',
+    },
+    image: {
+        width: 300,
+        height: 300,
+        left: 50,
+        top: 150,
+    },
+    homeButton: {
+        top: 390,
+        left: 40,
+        backgroundColor: 'white',
+        width: 60,
+        height: 60,
+    },
+    homeButtonImage: {
+        width: 60,
+        height: 60
+    }
     })
