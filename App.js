e0d94a94b033b640d6cb68520c9db3258a8da64c
1404pavani <pavani1404@utexas.edu>
2022-10-19 21:37:18 -0500

    message

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import DiscoveryScreen from "./screens/Discovery_Screen.js"
+import ArtistScreen from "./screens/Artist_Screen.js"
 
 export default function App() {
   return (
-    <DiscoveryScreen/>
+    <ArtistScreen/>
+    
   );
+
 }
 
 const styles = StyleSheet.create({
