8e84a0ac247e480986449a924156934970cb5079
matthew1 <gengarodyssey@gmail.com>
2022-11-12 14:01:49 -0600

    swiping stuff

--- frontend/screens/Screen_Scroller.js
+++ frontend/screens/Screen_Scroller.js
@@ -1,7 +1,6 @@
 import { StyleSheet, View, FlatList, Dimensions, ImageBackground, Text, TouchableOpacity } from 'react-native'
 import {useState} from 'react'
 import DiscoveryScreen from './Discovery_Screen'
-import ArtistScreen from './Artist_Screen'
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
 
@@ -16,6 +15,7 @@ const songs = [
     title: "title 2",
     uri: "uri 2"
   },
+
 ]
 
 const artist = [
@@ -26,24 +26,32 @@ const artist = [
 
 export default function ScreenScroller() {
 
-  const [horizontal, setHorizontal] = useState(false);
+  const [horizontal, setHorizontal] = useState(true);
+  const [snap, setWidth] = useState(Dimensions.get("window").width);
+
+
+
 
   return (
     <View style={styles.container}>
-      <GestureRecognizer
-        onSwipeLeft={(state) => setHorizontal(false)}
-      >
         <FlatList
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
           snapToAlignment='start'
-          snapToInterval={Dimensions.get("window").height}
+          snapToInterval={snap}
           decelerationRate='fast'
           data={songs}
           horizontal = {horizontal}
           renderItem={({ item }) => (
+            <GestureRecognizer
+            onSwipeLeft={(state) => {setHorizontal(true); setWidth(Dimensions.get("window").width);}}
+            onSwipeUp={(state) => {setHorizontal(false); setWidth(Dimensions.get("window").height);}}
+            onSwipeDown={(state) => {setHorizontal(false); setWidth(Dimensions.get("window").height);}}
+          >
             <DiscoveryScreen artist={item.artist} title={item.title} uri={item.title}/>
+            </GestureRecognizer>
           )}
         />
-        </GestureRecognizer>
       <ImageBackground 
             source={require("../assets/black-rectangle.png")}
             style={styles.rectangle}
@@ -65,8 +73,9 @@ export default function ScreenScroller() {
         </TouchableOpacity>
       
     </View>
-  )
+  );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -109,5 +118,5 @@ homeButtonImage: {
 profileButtonImage: {
     width: 25,
     height: 33
-}
+},
 })
