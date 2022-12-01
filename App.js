1d070423f0747a10b49908cce4280166c2a5bb34
1404pavani <pavani1404@utexas.edu>
2022-11-30 22:28:12 -0600

    3rd Artist screen

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import DiscoveryScreen from "./screens/Discovery_Screen.js"
 import ArtistScreen from "./screens/Artist_Screen.js"
 import Screen_Scroller from "./screens/Screen_Scroller.js"
 import ArtistTwo from "./screens/Artist2.js"
+import ArtistThree from "./screens/Artist3.js"
+
 
 
 export default function App() {
   return (
-    <ArtistTwo/>
+    <ArtistThree/>
     
   );
 
